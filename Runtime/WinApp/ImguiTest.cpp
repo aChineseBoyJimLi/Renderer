4cd1730a4cd86e689df618c1b7d2c14cfeee975c
D20_Rolling <794486346@qq.com>
2024-05-27 10:13:59 +0800

    imgui test

---
+++ Runtime/WinApp/ImguiTest.cpp
@@ -0,0 +1,9 @@
+﻿#include "ImguiTestApp.h"
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+
+bool ImguiTestApp::InitImgui()
+{
+    
+}
---
+++ Runtime/WinApp/ImguiTestApp.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+
+#include "../Core/Win32Base.h"
+#include "../RHI/RHI.h"
+#include "AssetsManager.h"
+
+#include "imgui.h"
+
+
+class ImguiTestApp : public Win32Base
+{
+public:
+    using Win32Base::Win32Base;
+
+protected:
+    bool Init() final;
+    void Tick()  final;
+    void Shutdown() final;
+    void OnBeginResize() override;
+    void OnResize(int width, int height) final;
+    void OnEndResize() final;
+
+private:
+    bool InitImgui();
+};
